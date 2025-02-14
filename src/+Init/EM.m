32265aed51276e5ebefb4ff6d57156bf7263ac97
Robert Lee <rob3rtyl33@gmail.com>
2025-02-13 20:34:17 -0500

    Updated EM parameters

--- src/+Init/EM.m
+++ src/+Init/EM.m
@@ -1,32 +1,8 @@
 %% EM.m
 % initialize Earth-Moon system parameters
 
-% Gm [km^3/s^2]
-%   source: supplementary material
-Gm_Earth    = 398600.4415;
-Gm_Moon     = 4902.8005821478;
-
-mu = @(m1,m2) m2/(m1+m2);
-mu = mu(Gm_Earth,Gm_Moon);
-
-% l* [km]
-%   source: supplementary material
-LU = 384400;
-LU2km = @(l_nd) l_nd*LU;
-
-% t* [s]
-TU = sqrt(LU^3/(Gm_Earth+Gm_Moon));
-TU2h = @(t_nd) t_nd*TU/3600;
-TU2d = @(t_nd) t_nd*TU/86400;
-TU2y = @(t_nd) TU2d(t_nd)/365.25;
-
-% radii [km] -> [LU]
-R_Earth = 6371.0/LU;
-R_Moon  = 1737.4/LU;
-
-% libration points
-L_pts = gen_L_pts(mu);
-
-% for plotting
-clr_Earth = "#559c33";
-clr_Moon = "#999693";
+p.mu = 0.01215058560962404;
+p.LU = 389703;
+p.TU = 382981;
+p.R_M = 1737.4/p.LU;
+p.R_E = 6371.0/p.LU;
